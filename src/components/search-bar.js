fbab7887c251b561376f4989943cc6f1d0852b2d
Les Ansley <les@ansleyfamily.com>
2023-01-26 23:23:31 +0200

    Build search bar and add styling

---
+++ src/components/search-bar.js
@@ -0,0 +1,91 @@
+import React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
+import Button from "@mui/material/Button";
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(1)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
+
+const SearchBar = () => {
+  const handleInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  const handleOnClick = (event) => {
+    console.log("clicked");
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar sx={{ alignItems: "left" }}>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
+          >
+            Starz 'n Carz
+          </Typography>
+          <Search sx={{ alignItems: "left" }}>
+            <StyledInputBase
+              onChange={handleInputChange}
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
+            <Button onClick={handleOnClick}>
+              <SearchIconWrapper>
+                <SearchIcon sx={{ fill: "white" }} />
+              </SearchIconWrapper>
+            </Button>
+          </Search>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default SearchBar;
