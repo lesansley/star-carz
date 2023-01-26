cadce24b73530ee68a675272bc7f090d6b309b3d
Les Ansley <les@ansleyfamily.com>
2023-01-26 13:28:27 +0200

    Create basic layout components

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import SearchBar from "./search-bar";
+
+const Header = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import "./index.css";
+import store from "./app/store";
+
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
