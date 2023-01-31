b3c3bae93a1a849ea3a0ebf6742fe4be962e5587
Les Ansley <les@ansleyfamily.com>
2023-01-31 12:54:21 +0200

    Remove logging

--- src/components/vehicle-details.js
+++ src/components/vehicle-details.js
@@ -3,7 +3,6 @@ import CardContent from "@mui/material/CardContent";
 import CardElement from "./card-element";
 
 const VehicleDetails = ({ data }) => {
-  console.log(data);
   const vehicle = (({
     name,
     model,
