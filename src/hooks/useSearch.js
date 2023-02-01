85e12188e777c234f55eb6006a8cf2802637fffb
Les Ansley <les@ansleyfamily.com>
2023-02-01 11:05:02 +0200

    Correct error message

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -4,7 +4,7 @@ import { SearchContext } from "../context";
 function useSearch() {
   const context = React.useContext(SearchContext);
   if (!context) {
-    throw new Error("useCount must be used within a CountProvider");
+    throw new Error("useSearch must be used within a SearchProvider");
   }
   return context;
 }
